becfaee3c9304af9c0a6cd62ff798a16428d5729
katsu560 <katsu560oo-@docomo.ne.jp>
2023-03-18 20:29:34 +0900

    change n_threads by environment variable GGML_NTHREADS or command line option

--- tests/test2.c
+++ tests/test2.c
@@ -20,7 +20,17 @@ int main(int argc, const char ** argv) {
     struct ggml_opt_params opt_params = ggml_opt_default_params(GGML_OPT_ADAM);
     opt_params.adam.alpha = 0.01f;
 
-    opt_params.n_threads = (argc > 1) ? atoi(argv[1]) : 8;
+    // original threads: 8
+    int nthreads = 8;
+    const char *env = getenv("GGML_NTHREADS");
+    if (env != NULL) {
+        nthreads = atoi(env);
+    }
+    if (argc > 1) {
+        nthreads = atoi(argv[1]);
+    }
+    opt_params.n_threads = nthreads;
+    printf("test2: n_threads:%d\n", opt_params.n_threads);
 
     const float xi[] = {  1.0f,  2.0f,  3.0f,  4.0f,  5.0f , 6.0f,  7.0f,  8.0f,  9.0f,  10.0f, };
           float yi[] = { 15.0f, 25.0f, 35.0f, 45.0f, 55.0f, 65.0f, 75.0f, 85.0f, 95.0f, 105.0f, };
