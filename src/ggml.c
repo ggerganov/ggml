2553e1a0db495ac1024ebaa9f39c3cd8dd4f2783
maihd <maihd.dev@gmail.com>
2023-03-23 14:13:38 +0700

    make it work on Windows

--- src/ggml.c
+++ src/ggml.c
@@ -30,6 +30,9 @@
 #include <errno.h>
 #endif
 
+// Need this to compile with Visual Studio 2017
+#define restrict __restrict
+
 typedef volatile LONG atomic_int;
 typedef atomic_int atomic_bool;
 
@@ -1485,6 +1488,9 @@ struct ggml_context * ggml_init(struct ggml_init_params params) {
     static bool is_first_call = true;
 
     if (is_first_call) {
+        // initialize time system (required on Windows)
+        ggml_time_init();
+
         // initialize GELU, EXP and F32 tables
         {
             const uint64_t t_start = ggml_time_us(); UNUSED(t_start);
