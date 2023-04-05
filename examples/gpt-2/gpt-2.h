a5238962f0a1fc983388700ebc6dd903a11cb352
pikalover6 <khoward5241@student.pps.net>
2023-04-04 23:11:26 -0700

    Finish gpt-2 and gpt-j API's

---
+++ examples/gpt-2/gpt-2.h
@@ -0,0 +1,76 @@
+#ifndef GPT2_H
+#define GPT2_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "ggml/ggml.h"
+#include "utils.h"
+
+// default hparams (GPT-2 117M)
+struct gpt2_hparams {
+    int32_t n_vocab = 50257;
+    int32_t n_ctx   = 1024;
+    int32_t n_embd  = 768;
+    int32_t n_head  = 12;
+    int32_t n_layer = 12;
+    int32_t f16     = 1;
+};
+
+struct gpt2_layer {
+    // normalization
+    struct ggml_tensor * ln_1_g;
+    struct ggml_tensor * ln_1_b;
+
+    struct ggml_tensor * ln_2_g;
+    struct ggml_tensor * ln_2_b;
+
+    // attention
+    struct ggml_tensor * c_attn_attn_w;
+    struct ggml_tensor * c_attn_attn_b;
+
+    struct ggml_tensor * c_attn_proj_w;
+    struct ggml_tensor * c_attn_proj_b;
+
+    // mlp
+    struct ggml_tensor * c_mlp_fc_w;
+    struct ggml_tensor * c_mlp_fc_b;
+
+    struct ggml_tensor * c_mlp_proj_w;
+    struct ggml_tensor * c_mlp_proj_b;
+};
+
+struct gpt2_model {
+    gpt2_hparams hparams;
+
+    // normalization
+    struct ggml_tensor * ln_f_g;
+    struct ggml_tensor * ln_f_b;
+
+    struct ggml_tensor * wte;     // position embedding
+    struct ggml_tensor * wpe;     //    token embedding
+    struct ggml_tensor * lm_head; // language model head
+
+    std::vector<gpt2_layer> layers;
+
+    // key + value memory
+    struct ggml_tensor * memory_k;
+    struct ggml_tensor * memory_v;
+
+    //
+    struct ggml_context * ctx;
+    std::map<std::string, struct ggml_tensor *> tensors;
+};
+
+bool gpt2_model_load(const std::string &fname, gpt2_model &model, gpt_vocab &vocab);
+
+bool gpt2_eval(
+    const gpt2_model & model,
+    const int n_threads,
+    const int n_past,
+    const std::vector<gpt_vocab::id> & embd_inp,
+          std::vector<float>         & embd_w,
+          size_t                     & mem_per_token);
+
+#endif // GPT2_H
