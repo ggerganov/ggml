2e09d7c971809e2b3ca342fd7fc1c7538a1e245b
Georgi Gerganov <ggerganov@gmail.com>
2023-04-29 12:33:57 +0300

    scripts : add sync-whisper.sh

---
+++ scripts/sync-whisper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -rpv ../whisper.cpp/ggml.c       src/ggml.c
+cp -rpv ../whisper.cpp/ggml-cuda.cu src/ggml-cuda.cu
+cp -rpv ../whisper.cpp/ggml-cuda.h  src/ggml-cuda.h
+cp -rpv ../whisper.cpp/ggml.h       include/ggml/ggml.h
