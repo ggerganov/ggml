16244362ed1177f87fac2e3c3e509fd2ef80e295
Georgi Gerganov <ggerganov@gmail.com>
2023-05-02 21:27:02 +0300

    scripts : update sync scripts

--- scripts/sync-whisper.sh
+++ scripts/sync-whisper.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cp -rpv ../whisper.cpp/ggml.c                         src/ggml.c
-cp -rpv ../whisper.cpp/ggml-cuda.cu                   src/ggml-cuda.cu
 cp -rpv ../whisper.cpp/ggml-cuda.h                    src/ggml-cuda.h
+cp -rpv ../whisper.cpp/ggml-cuda.cu                   src/ggml-cuda.cu
+cp -rpv ../whisper.cpp/ggml-opencl.h                  src/ggml-opencl.h
+cp -rpv ../whisper.cpp/ggml-opencl.c                  src/ggml-opencl.c
 cp -rpv ../whisper.cpp/ggml.h                         include/ggml/ggml.h
 cp -rpv ../whisper.cpp/examples/common-ggml.h         examples/common-ggml.h
 cp -rpv ../whisper.cpp/examples/common-ggml.cpp       examples/common-ggml.cpp
