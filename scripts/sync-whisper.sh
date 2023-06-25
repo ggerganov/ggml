a79d6779229533714649cafbf095d67f5198dfae
Georgi Gerganov <ggerganov@gmail.com>
2023-06-25 15:37:02 +0300

    opencl : remove ggml-opencl.c

--- scripts/sync-whisper.sh
+++ scripts/sync-whisper.sh
@@ -4,7 +4,7 @@ cp -rpv ../whisper.cpp/ggml.c                         src/ggml.c
 cp -rpv ../whisper.cpp/ggml-cuda.h                    src/ggml-cuda.h
 cp -rpv ../whisper.cpp/ggml-cuda.cu                   src/ggml-cuda.cu
 cp -rpv ../whisper.cpp/ggml-opencl.h                  src/ggml-opencl.h
-cp -rpv ../whisper.cpp/ggml-opencl.c                  src/ggml-opencl.c
+cp -rpv ../whisper.cpp/ggml-opencl.cpp                src/ggml-opencl.cpp
 cp -rpv ../whisper.cpp/ggml.h                         include/ggml/ggml.h
 cp -rpv ../whisper.cpp/examples/common.h              examples/common.h
 cp -rpv ../whisper.cpp/examples/common.cpp            examples/common.cpp
