a5247db3177b54654411984f3cd74c8e7d531c58
Georgi Gerganov <ggerganov@gmail.com>
2023-07-15 16:26:23 +0300

    ci : try fix pipefail

--- ci/run.sh
+++ ci/run.sh
@@ -15,14 +15,12 @@ function gg_ci_0 {
     cd build-ci-0
 
     set -e
-    set -o pipefail
 
     time cmake -DCMAKE_BUILD_TYPE=Debug .. | tee $OUT/ci-0-cmake.log
     time make -j4 | tee $OUT/ci-0-make.log
 
     time ctest -E test-opt | tee $OUT/ci-0-ctest.log
 
-    set +o pipefail
     set +e
 }
 
@@ -33,20 +31,20 @@ function gg_ci_1 {
     cd build-ci-1
 
     set -e
-    set -o pipefail
 
     time cmake -DCMAKE_BUILD_TYPE=Debug .. | tee $OUT/ci-1-cmake.log
     time make -j4 | tee $OUT/ci-1-make.log
 
     time ctest | tee $OUT/ci-1-ctest.log
 
-    set +o pipefail
     set +e
 }
 
 ## main
 
 ret=0
+
+set -o pipefail
 
 gg_ci_0 | tee $OUT/ci-0.log
 cur=$?
@@ -57,5 +55,7 @@ gg_ci_1 | tee $OUT/ci-1.log
 cur=$?
 echo "$cur" > $OUT/ci-1.exit
 ret=$(($ret + $cur))
+
+set +o pipefail
 
 exit $ret
