df4ca67f2adbf64df0f660e799f6b915eadc68e8
Georgi Gerganov <ggerganov@gmail.com>
2023-07-16 20:03:35 +0300

    ci : add mpt

--- ci/run.sh
+++ ci/run.sh
@@ -51,7 +51,7 @@ function gg_run {
 # ctest_debug
 
 function gg_run_ctest_debug {
-    cd $SRC
+    cd ${SRC}
 
     rm -rf build-ci-debug && mkdir build-ci-debug && cd build-ci-debug
 
@@ -78,7 +78,7 @@ function gg_sum_ctest_debug {
 # ctest_release
 
 function gg_run_ctest_release {
-    cd $SRC
+    cd ${SRC}
 
     rm -rf build-ci-release && mkdir build-ci-release && cd build-ci-release
 
@@ -104,7 +104,7 @@ function gg_sum_ctest_release {
 # gpt_2
 
 function gg_run_gpt_2 {
-    cd $SRC
+    cd ${SRC}
 
     gg_wget models/gpt-2 https://huggingface.co/ggerganov/ggml/resolve/main/ggml-model-gpt-2-117M.bin
 
@@ -131,12 +131,63 @@ function gg_sum_gpt_2 {
     gg_printf '```\n'
 }
 
+# mpt
+
+function gg_run_mpt {
+    cd ${SRC}
+
+    gg_wget models/mpt/7B/ https://huggingface.co/mosaicml/mpt-7b/raw/main/tokenizer.json
+    gg_wget models/mpt/7B/ https://huggingface.co/mosaicml/mpt-7b/raw/main/tokenizer_config.json
+    gg_wget models/mpt/7B/ https://huggingface.co/mosaicml/mpt-7b/raw/main/pytorch_model.bin.index.json
+    gg_wget models/mpt/7B/ https://huggingface.co/mosaicml/mpt-7b/resolve/main/pytorch_model-00001-of-00002.bin
+    gg_wget models/mpt/7B/ https://huggingface.co/mosaicml/mpt-7b/resolve/main/pytorch_model-00002-of-00002.bin
+
+    cd build-ci-release
+
+    set -e
+
+    path_models="../models/mpt/7B"
+    model_f16="${path_models}/ggml-model-f16.bin"
+    model_q4_0="${path_models}/ggml-model-q4_0.bin"
+
+    python3 ../examples/mpt/7B/convert-h5-to-ggml.py ${path_models} 1
+    ./bin/mpt-quantize ${model_f16} ${model_q4_0} q4_0
+
+    (time ./bin/mpt --model ${model_f16} -s 1234 -n 64 -t 8 -p "I believe the meaning of life is") 2>&1 | tee -a $OUT/${ci}-tg.log
+    (time ./bin/mpt --model ${model_q4_0} -s 1234 -n 64 -t 8 -p "I believe the meaning of life is") 2>&1 | tee -a $OUT/${ci}-tg.log
+
+    set +e
+}
+
+function gg_sum_mpt {
+    gg_printf '### %s\n\n' "${ci}"
+
+    gg_printf 'Runs short MPT text generation\n'
+    gg_printf '- status: %s\n' "$(cat $OUT/${ci}.exit)"
+    gg_printf '```\n'
+    gg_printf '%s\n' "$(cat $OUT/${ci}-tg.log)"
+    gg_printf '```\n'
+}
+
 ## main
+
+if [ -z $GG_BUILD_LOW_PERF ]; then
+    rm -rf ${SRC}/models
+
+    mkdir -p ${GG_GGML_MNT}/models
+    ln -sfn ${GG_GGML_MNT}/models ${SRC}/models
+
+    python3 -m pip install -r ${SRC}/requirements.txt
+fi
 
 ret=0
 
 test $ret -eq 0 && gg_run ctest_debug
 test $ret -eq 0 && gg_run ctest_release
 test $ret -eq 0 && gg_run gpt_2
+
+if [ -z $GG_BUILD_LOW_PERF ]; then
+    test $ret -eq 0 && gg_run mpt
+fi
 
 exit $ret
