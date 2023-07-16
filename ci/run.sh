1708e8f04725df2630d8da6425693a8ff70c97e2
Georgi Gerganov <ggerganov@gmail.com>
2023-07-16 17:59:29 +0300

    ci : try to fix duplicated output

--- ci/run.sh
+++ ci/run.sh
@@ -112,8 +112,7 @@ function gg_run_gpt_2 {
 
     cmake -DCMAKE_BUILD_TYPE=Release .. && make -j4
 
-    time ./bin/gpt-2 --model ../models/gpt-2/ggml-model-gpt-2-117M.bin -s 1234 -n 64 -t 4 > $OUT/${ci}.log 2>&1
-    cat $OUT/${ci}.log
+    (./bin/gpt-2 --model ../models/gpt-2/ggml-model-gpt-2-117M.bin -s 1234 -n 64 -t 4 ) | tee $OUT/${ci}.log
 
     set +e
 }
