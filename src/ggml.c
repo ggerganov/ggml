e1ed37317dfd156174625ddd3b6a3aba35d828a4
Georgi Gerganov <ggerganov@gmail.com>
2023-07-23 19:30:32 +0300

    ggml : fix assert number of ops

--- src/ggml.c
+++ src/ggml.c
@@ -3803,7 +3803,7 @@ static const char * GGML_OP_NAME[GGML_OP_COUNT] = {
     "CROSS_ENTROPY_LOSS_BACK",
 };
 
-static_assert(GGML_OP_COUNT == 59, "GGML_OP_COUNT != 68");
+static_assert(GGML_OP_COUNT == 59, "GGML_OP_COUNT != 59");
 
 static const char * GGML_OP_SYMBOL[GGML_OP_COUNT] = {
     "none",
