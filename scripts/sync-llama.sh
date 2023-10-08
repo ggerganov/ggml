2da43c20046f66aff023cc14ef5c4ce33ec3cce4
Georgi Gerganov <ggerganov@gmail.com>
2023-10-08 19:44:14 +0300

    sync : llama.cpp (Metal + OpenCL + minor alibi) (#558)
    ggml-ci

--- scripts/sync-llama.sh
+++ scripts/sync-llama.sh
@@ -2,6 +2,7 @@
 
 cp -rpv ../llama.cpp/ggml.c           src/ggml.c
 cp -rpv ../llama.cpp/ggml-alloc.c     src/ggml-alloc.c
+cp -rpv ../llama.cpp/ggml-backend.c   src/ggml-backend.c
 cp -rpv ../llama.cpp/ggml-cuda.h      src/ggml-cuda.h
 cp -rpv ../llama.cpp/ggml-cuda.cu     src/ggml-cuda.cu
 cp -rpv ../llama.cpp/ggml-opencl.h    src/ggml-opencl.h
@@ -11,6 +12,7 @@ cp -rpv ../llama.cpp/ggml-metal.m     src/ggml-metal.m
 cp -rpv ../llama.cpp/ggml-metal.metal src/ggml-metal.metal
 cp -rpv ../llama.cpp/ggml.h           include/ggml/ggml.h
 cp -rpv ../llama.cpp/ggml-alloc.h     include/ggml/ggml-alloc.h
+cp -rpv ../llama.cpp/ggml-backend.h   include/ggml/ggml-backend.h
 
 cp -rpv ../llama.cpp/tests/test-opt.cpp           tests/test-opt.cpp
 cp -rpv ../llama.cpp/tests/test-grad0.cpp         tests/test-grad0.cpp
