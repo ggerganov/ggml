574735c3d2aba284c34899aebc4fb336fcb23e35
FSSRepo <go778sgt@gmail.com>
2023-10-09 15:51:49 -0400

    add conv1d and conv2d unit tests

---
+++ tests/test-conv2d.cpp
@@ -0,0 +1,255 @@
+#include "ggml.h"
+#include "ggml/ggml-alloc.h"
+#include "ggml/ggml-backend.h"
+
+//#define GGML_USE_CUBLAS
+
+#ifdef GGML_USE_CUBLAS
+#include "ggml-cuda.h"
+#endif
+
+#ifdef GGML_USE_METAL
+#include "ggml-metal.h"
+#endif
+
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+
+struct test_model {
+    struct ggml_tensor * a;
+    struct ggml_tensor * b;
+    ggml_backend_t backend = NULL;
+    ggml_backend_buffer_t buffer;
+    struct ggml_context * ctx;
+};
+
+void load_model(test_model & model, bool use_gpu = false) {
+    // create data
+    int KW = 3, KH = 3, IC = 4, OC = 4;
+    int IW = 4, IH = 4, /* IC = 640 */ N = 1;
+
+    // Initialize adata
+    float* adata = new float[KW * KH * IC * OC];
+    for (size_t i = 0; i < KW * KH * IC * OC; i++) {
+        adata[i] = 2.0f;
+    }
+
+    // Convert adata to fp16 format
+    uint16_t* hadata = new uint16_t[KW * KH * IC * OC];
+    ggml_fp32_to_fp16_row(adata, hadata, KW * KH * IC * OC);
+
+    // Initialize bdata
+    float* bdata =  new float[IW * IH * IC * N];
+    for (size_t i = 0; i < IW * IH * IC * N; i++) {
+        bdata[i] = 1.0f;
+    }
+
+    size_t buffer_size = 0;
+    {
+        buffer_size += KW * KH * IC * OC * ggml_type_sizef(GGML_TYPE_F16); // tensor a
+        buffer_size += IW * IH * IC * N * ggml_type_sizef(GGML_TYPE_F32); // tensor b
+        buffer_size += 1024; // overhead
+    }
+
+    printf("%s: ggml tensor size    = %d bytes\n", __func__, (int) sizeof(ggml_tensor));
+    printf("%s: backend buffer size = %0.2f MB\n", __func__, (int) (buffer_size/ 1024.f/ 1024.f));
+
+    int num_tensors = 2;
+    struct ggml_init_params params {
+            /*.mem_size   =*/ ggml_tensor_overhead() * num_tensors,
+            /*.mem_buffer =*/ NULL,
+            /*.no_alloc   =*/ true,
+    };
+
+    // initialize the backend
+#ifdef GGML_USE_CUBLAS
+    if (use_gpu) {
+        fprintf(stderr, "%s: using CUDA backend\n", __func__);
+        model.backend = ggml_backend_cuda_init();
+        if (!model.backend) {
+            fprintf(stderr, "%s: ggml_backend_cuda_init() failed\n", __func__);
+        }
+    }
+#endif
+
+#ifdef GGML_USE_METAL
+    if (use_gpu) {
+        fprintf(stderr, "%s: using Metal backend\n", __func__);
+        ggml_metal_log_set_callback(ggml_log_callback_default, nullptr);
+        model.backend = ggml_backend_metal_init();
+        if (!model.backend) {
+            fprintf(stderr, "%s: ggml_backend_metal_init() failed\n", __func__);
+        }
+    }
+#endif
+
+    if(!model.backend) {
+        // fallback to CPU backend
+        model.backend = ggml_backend_cpu_init();
+    }
+
+    model.buffer = ggml_backend_alloc_buffer(model.backend, buffer_size);
+
+    // create context
+    model.ctx = ggml_init(params);
+
+    // create tensors
+    model.a = ggml_new_tensor_4d(model.ctx, GGML_TYPE_F16,  KW, KH, IC, OC);
+    model.b = ggml_new_tensor_4d(model.ctx, GGML_TYPE_F32, IW, IH, IC, N);
+
+    // create a allocator
+    ggml_allocr * alloc = ggml_allocr_new_from_buffer(model.buffer);
+
+    // alloc memory
+    ggml_allocr_alloc(alloc, model.a);
+
+    // load data to buffer
+    if(ggml_backend_is_cpu(model.backend)) {
+        memcpy(model.a->data, hadata, ggml_nbytes(model.a));
+    } else {
+        ggml_backend_tensor_set(model.a, hadata, 0, ggml_nbytes(model.a));
+    }
+
+    // alloc memory
+    ggml_allocr_alloc(alloc, model.b);
+
+    if(ggml_backend_is_cpu(model.backend)
+#ifdef GGML_USE_METAL
+                || ggml_backend_is_metal(model.backend)
+#endif
+    ) {
+        memcpy(model.b->data, bdata, ggml_nbytes(model.b));
+    } else {
+        ggml_backend_tensor_set(model.b, bdata, 0, ggml_nbytes(model.b));
+    }
+
+    ggml_allocr_free(alloc);
+}
+
+struct ggml_cgraph * build_graph(const test_model& model, struct ggml_allocr * allocr) {
+    static size_t buf_size = ggml_tensor_overhead()*GGML_MAX_NODES + ggml_graph_overhead();
+    static std::vector<uint8_t> buf(buf_size);
+
+    struct ggml_init_params params0 = {
+        /*.mem_size   =*/ buf_size,
+        /*.mem_buffer =*/ buf.data(),
+        /*.no_alloc   =*/ true, // the tensors will be allocated later by ggml_allocr_alloc_graph()
+    };
+
+    // create a temporally context to build the graph
+    struct ggml_context * ctx0 = ggml_init(params0);
+
+    struct ggml_cgraph  * gf = ggml_new_graph(ctx0);
+
+    int s0 = 1;
+    int s1 = 1;
+    int p0 = 1;
+    int p1 = 1;
+    int d0 = 1;
+    int d1 = 1;
+
+    struct ggml_tensor* result = ggml_conv_2d(ctx0, model.a, model.b, s0, s1, p0, p1, d0, d1);
+
+    ggml_build_forward_expand(gf, result);
+
+    // delete the temporally context used to build the graph
+    ggml_free(ctx0);
+    return gf;
+}
+
+struct ggml_tensor* compute(const test_model & model, struct ggml_allocr * allocr) {
+    // reset the allocator to free all the memory allocated during the previous inference
+    ggml_allocr_reset(allocr);
+
+    struct ggml_cgraph * gf = build_graph(model, allocr);
+
+    // allocate tensors
+    ggml_allocr_alloc_graph(allocr, gf);
+    int n_threads = 1;
+
+    if (ggml_backend_is_cpu(model.backend)) {
+        ggml_backend_cpu_set_n_threads(model.backend, n_threads);
+    }
+
+#ifdef GGML_USE_METAL
+    if (ggml_backend_is_metal(model.backend)) {
+        ggml_backend_metal_set_n_cb(model.backend, n_threads);
+    }
+#endif
+
+    ggml_backend_graph_compute(model.backend, gf);
+
+    //ggml_graph_print(gf);
+
+    // in this case, the output tensor is the last one in the graph
+    return gf->nodes[gf->n_nodes - 1];
+}
+
+int main(void)
+{
+    ggml_time_init();
+
+    test_model model;
+    load_model(model, true);
+
+    ggml_backend_buffer_t buf_compute; // for compute
+    struct ggml_allocr * allocr = NULL;
+
+    {
+        size_t align = ggml_backend_get_alignment(model.backend);
+        allocr = ggml_allocr_new_measure(align);
+
+        //create the worst case graph for memory usage estimation
+        struct ggml_cgraph * gf = build_graph(model, allocr);
+        size_t mem_size = ggml_allocr_alloc_graph(allocr, gf);
+        ggml_allocr_free(allocr);
+
+        // compute the required memory
+        buf_compute = ggml_backend_alloc_buffer(model.backend, mem_size);
+        allocr = ggml_allocr_new_from_buffer(buf_compute);
+        fprintf(stderr, "%s: compute buffer size: %.2f MB\n", __func__, mem_size/1024.0f/1024.0f);
+    }
+
+    struct ggml_tensor * result = compute(model, allocr);
+
+    float* out_data = new float[ggml_nelements(result)];
+
+    ggml_backend_tensor_get(result, out_data, 0, ggml_nbytes(result));
+
+    const int n_test = 64;
+
+    float expected [n_test] = {
+        32.00f, 48.00f, 48.00f, 32.00f, 48.00f, 72.00f,
+        72.00f, 48.00f, 48.00f, 72.00f, 72.00f, 48.00f,
+        32.00f, 48.00f, 48.00f, 32.00f, 32.00f, 48.00f,
+        48.00f, 32.00f, 48.00f, 72.00f, 72.00f, 48.00f,
+        48.00f, 72.00f, 72.00f, 48.00f, 32.00f, 48.00f,
+        48.00f, 32.00f, 32.00f, 48.00f, 48.00f, 32.00f,
+        48.00f, 72.00f, 72.00f, 48.00f, 48.00f, 72.00f,
+        72.00f, 48.00f, 32.00f, 48.00f, 48.00f, 32.00f,
+        32.00f, 48.00f, 48.00f, 32.00f, 48.00f, 72.00f,
+        72.00f, 48.00f, 48.00f, 72.00f, 72.00f, 48.00f,
+        32.00f, 48.00f, 48.00f, 32.00f };
+
+    bool passed = true;
+    for(int i = 0; i < n_test; i++) {
+        if(out_data[i] != expected[i]) {
+            passed = false;
+            break;
+        }
+    }
+
+    printf("ggml_conv2d (%i): %s", ggml_nelements(result), passed && (ggml_nelements(result) == n_test) ? "PASS" : "FAILED");
+    ggml_free(model.ctx);
+
+    ggml_backend_buffer_free(model.buffer);
+    ggml_backend_buffer_free(buf_compute);
+    ggml_backend_free(model.backend);
+    return 0;
+}
