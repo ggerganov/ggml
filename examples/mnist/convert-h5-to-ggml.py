74f62c1aaf383ff2c98ef331291a11147c294591
Georgi Gerganov <ggerganov@gmail.com>
2024-05-11 16:42:01 +0300

    mnist : clean whitespace
    ggml-ci

--- examples/mnist/convert-h5-to-ggml.py
+++ examples/mnist/convert-h5-to-ggml.py
@@ -18,7 +18,6 @@ import json
 import numpy as np
 import re
 
-
 import torch
 import torch.nn as nn
 import torchvision.datasets as dsets
@@ -45,11 +44,11 @@ fout.write(struct.pack("i", 0x67676d6c)) # magic: ggml in hex
 
 for name in list_vars.keys():
     data = list_vars[name].squeeze().numpy()
-    print("Processing variable: " + name + " with shape: ", data.shape) 
+    print("Processing variable: " + name + " with shape: ", data.shape)
     n_dims = len(data.shape);
-   
+
     fout.write(struct.pack("i", n_dims))
-    
+
     data = data.astype(np.float32)
     for i in range(n_dims):
         fout.write(struct.pack("i", data.shape[n_dims - 1 - i]))
