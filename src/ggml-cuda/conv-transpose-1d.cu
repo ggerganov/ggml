7301716d9de368608f17e9416a74f5a46357f489
slaren <slarengh@gmail.com>
2024-07-02 17:58:05 +0200

    style and warning fixes

--- src/ggml-cuda/conv-transpose-1d.cu
+++ src/ggml-cuda/conv-transpose-1d.cu
@@ -15,28 +15,21 @@ static  __global__ void conv_transpose_1d_kernel(
 
     float accumulator = 0;
 
-    for (int c = 0; c < src0_ne2; c++)
-    {
-
+    for (int c = 0; c < src0_ne2; c++) {
         int idx = global_index % dst_ne0;
 
         int kernel_offset = (src0_ne0 * src0_ne1 * c) + (out_index * src0_ne0);
         int input_offset = src1_ne0 * c;
 
-        int initial_weight_idx = idx > src0_ne0 -1 ? src0_ne0-1 : idx;
-
-        for (int i = 0; i < src1_ne0; i++)
-        {
-            if (!(idx >= i*s0 && idx < i*s0 + src0_ne0))
-            {
+        for (int i = 0; i < src1_ne0; i++) {
+            if (!(idx >= i*s0 && idx < i*s0 + src0_ne0)) {
                 continue;
             }
             int weight_idx = idx - i*s0;
 
-         
             float kernel_weight = src0[kernel_offset + weight_idx];
             float input_value =  src1[input_offset+i];
-          
+
             accumulator += kernel_weight * input_value;
         }
     }
@@ -52,11 +45,12 @@ static void conv_transpose_1d_f32_f32_cuda(
         cudaStream_t stream) {
 
     const int num_blocks = (output_size + CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE - 1) / CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE;
-    conv_transpose_1d_kernel<<<num_blocks,CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE, 0, stream>>>(s0,p0,d0,output_size,
-    src0_ne0, src0_ne1,  src0_ne2, src0_ne3,
-    src1_ne0, src1_ne1,  src1_ne2, src1_ne3,
-    dst_ne0,  dst_ne1,   dst_ne2,  dst_ne3,
-  src0,src1, dst);
+    conv_transpose_1d_kernel<<<num_blocks,CUDA_CONV_TRANPOSE_1D_BLOCK_SIZE, 0, stream>>>(
+        s0,p0,d0,output_size,
+        src0_ne0, src0_ne1,  src0_ne2, src0_ne3,
+        src1_ne0, src1_ne1,  src1_ne2, src1_ne3,
+        dst_ne0,  dst_ne1,   dst_ne2,  dst_ne3,
+        src0,src1, dst);
 }
 
 void ggml_cuda_op_conv_transpose_1d(ggml_backend_cuda_context & ctx, ggml_tensor * dst) {
@@ -71,25 +65,23 @@ void ggml_cuda_op_conv_transpose_1d(ggml_backend_cuda_context & ctx, ggml_tensor
 
     GGML_ASSERT(src0->type == GGML_TYPE_F32);
     GGML_ASSERT( dst->type == GGML_TYPE_F32);
-    
+
     GGML_ASSERT(ggml_is_contiguous(src0));
     GGML_ASSERT(ggml_is_contiguous(src1));
 
-
     const int32_t * opts = (const int32_t *)dst->op_params;
 
-    const int s0 = dst->op_params[0];
+    const int s0 = opts[0];
     const int p0 = 0;//opts[3];
     const int d0 = 1;//opts[4];
 
     const int64_t kernel_size = ggml_nelements(src0);
     const int64_t input_size = ggml_nelements(src1);
-    const int64_t output_size =  ggml_nelements(dst);
+    const int64_t output_size = ggml_nelements(dst);
 
-
-    conv_transpose_1d_f32_f32_cuda( s0,p0,d0,output_size, 
-    src0->ne[0],src0->ne[1],src0->ne[2],src0->ne[3],
-    src1->ne[0],src1->ne[1],src1->ne[2],src1->ne[3],
-    dst->ne[0],dst->ne[1],dst->ne[2],dst->ne[3],
-    src0_d, src1_d, dst_d, stream);
+    conv_transpose_1d_f32_f32_cuda(s0, p0, d0, output_size,
+        src0->ne[0], src0->ne[1], src0->ne[2], src0->ne[3],
+        src1->ne[0], src1->ne[1], src1->ne[2], src1->ne[3],
+        dst->ne[0], dst->ne[1], dst->ne[2], dst->ne[3],
+        src0_d, src1_d, dst_d, stream);
 }
