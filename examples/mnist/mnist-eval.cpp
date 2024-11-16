0ce22266d94463f0e93093ba418de7b8be77cb55
Johannes Gäßler <johannesg@5d6.de>
2024-11-16 13:49:35 +0100

    ggml: new optimization interface (#988)
    * ggml: new optimization interface
    
    remove test2.c, test3.c
    
    store adamw params in tensor
    
    move grads from tensor to graph
    
    * avoid segfault upon API misuse
    
    * add ggml-opt.h to public headers
    
    * remove dependence of ggml-opt.cpp on ggml-cpu.h

--- examples/mnist/mnist-eval.cpp
+++ examples/mnist/mnist-eval.cpp
@@ -1,4 +1,5 @@
 #include "ggml.h"
+#include "ggml-opt.h"
 
 #include "mnist-common.h"
 
@@ -24,7 +25,7 @@ int main(int argc, char ** argv) {
         exit(1);
     }
 
-    struct mnist_dataset dataset(/*nex =*/ MNIST_NTEST, /*shard_size =*/ MNIST_NBATCH_PHYSICAL);
+    ggml_opt_dataset_t dataset = ggml_opt_dataset_init(MNIST_NINPUT, MNIST_NCLASSES, MNIST_NTEST, MNIST_NBATCH_PHYSICAL);
 
     if (!mnist_image_load(argv[2], dataset)) {
         return 1;
@@ -36,46 +37,31 @@ int main(int argc, char ** argv) {
     const int iex = rand() % MNIST_NTEST;
     mnist_image_print(stdout, dataset, iex);
 
-    const std::string backend = argc >= 5 ? argv[4] : "CPU";
-
-    mnist_eval_result result_eval;
-
-    if (backend == "CPU") {
-        const int ncores_logical = std::thread::hardware_concurrency();
-        result_eval = mnist_graph_eval(
-            argv[1], ggml_get_data_f32(dataset.data), ggml_get_data_f32(dataset.labels), MNIST_NTEST, std::min(ncores_logical, (ncores_logical + 4)/2));
-        if (result_eval.success) {
-            fprintf(stdout, "%s: predicted digit is %d\n", __func__, result_eval.pred[iex]);
-
-            std::pair<double, double> result_loss = mnist_loss(result_eval);
-            fprintf(stdout, "%s: test_loss=%.6lf+-%.6lf\n", __func__, result_loss.first, result_loss.second);
-
-            std::pair<double, double> result_acc = mnist_accuracy(result_eval);
-            fprintf(stdout, "%s: test_acc=%.2lf+-%.2lf%%\n", __func__, 100.0*result_acc.first, 100.0*result_acc.second);
-
-            return 0;
-        }
-    } else {
-        fprintf(stdout, "%s: not trying to load a GGML graph from %s because this is only supported for the CPU backend\n", __func__, argv[1]);
-    }
+    const std::string backend = argc >= 5 ? argv[4] : "";
 
     const int64_t t_start_us = ggml_time_us();
-
-    mnist_model model = mnist_model_init_from_file(argv[1], backend);
-
-    mnist_model_build(model, MNIST_NBATCH_LOGICAL, MNIST_NBATCH_PHYSICAL);
-
+    mnist_model model = mnist_model_init_from_file(argv[1], backend, MNIST_NBATCH_LOGICAL, MNIST_NBATCH_PHYSICAL);
+    mnist_model_build(model);
     const int64_t t_load_us = ggml_time_us() - t_start_us;
-
     fprintf(stdout, "%s: loaded model in %.2lf ms\n", __func__, t_load_us / 1000.0);
-    result_eval = mnist_model_eval(model, dataset);
-    fprintf(stdout, "%s: predicted digit is %d\n", __func__, result_eval.pred[iex]);
 
-    std::pair<double, double> result_loss = mnist_loss(result_eval);
-    fprintf(stdout, "%s: test_loss=%.6lf+-%.6lf\n", __func__, result_loss.first, result_loss.second);
+    ggml_opt_result_t result_eval = mnist_model_eval(model, dataset);
 
-    std::pair<double, double> result_acc = mnist_accuracy(result_eval);
-    fprintf(stdout, "%s: test_acc=%.2lf+-%.2lf%%\n", __func__, 100.0*result_acc.first, 100.0*result_acc.second);
+    std::vector<int32_t> pred(MNIST_NTEST);
+    ggml_opt_result_pred(result_eval, pred.data());
+    fprintf(stdout, "%s: predicted digit is %d\n", __func__, pred[iex]);
+
+    double loss;
+    double loss_unc;
+    ggml_opt_result_loss(result_eval, &loss, &loss_unc);
+    fprintf(stdout, "%s: test_loss=%.6lf+-%.6lf\n", __func__, loss, loss_unc);
+
+    double accuracy;
+    double accuracy_unc;
+    ggml_opt_result_accuracy(result_eval, &accuracy, &accuracy_unc);
+    fprintf(stdout, "%s: test_acc=%.2lf+-%.2lf%%\n", __func__, 100.0*accuracy, 100.0*accuracy_unc);
+
+    ggml_opt_result_free(result_eval);
 
     return 0;
 }
