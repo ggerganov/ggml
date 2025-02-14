025eb3123ac6b53e5015ad5ec2ff364764708cfa
Takashi Okamoto <toraneko@gmail.com>
2025-02-14 01:55:50 +0000

    tests: add vulkan configuration in tests

--- tests/test-conv2d.cpp
+++ tests/test-conv2d.cpp
@@ -11,6 +11,10 @@
 #include "ggml-metal.h"
 #endif
 
+#ifdef GGML_USE_VULKAN
+#include "ggml-vulkan.h"
+#endif
+
 #include <cassert>
 #include <cmath>
 #include <cstdio>
@@ -94,6 +98,16 @@ void load_model(test_model & model, bool use_gpu = false) {
             fprintf(stderr, "%s: ggml_backend_metal_init() failed\n", __func__);
         }
     }
+#endif
+
+#ifdef GGML_USE_VULKAN
+        if (use_gpu) {
+            fprintf(stderr, "%s: using Vulkan backend\n", __func__);
+            model.backend = ggml_backend_vk_init(0);
+            if (!model.backend) {
+                fprintf(stderr, "%s: ggml_backend_vk_init() failed\n", __func__);
+            }
+        }
 #endif
 
     if(!model.backend) {
